14d44b8a32701461dd067913157e532951fed63e
Tibo Van Gindertaelen <tibo.vangindertaelen@student.odisee.be>
2020-10-27 13:50:33 +0100

    Added Folder for the new React Course  Branch

---
+++ 5-React-Course/react-complete-guide/src/Person/Person.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// ES6 approach - BPE
+const person = (props) => {
+    return (
+    <div>
+        <p>I'm a Person and I'm {props.name} and I'm {props.age} years old!</p>
+        <p>{props.children}</p>
+    </div>
+    )
+};
+
+export default person
