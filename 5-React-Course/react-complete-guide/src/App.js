366686f61507f33444e76cf9f3d23d5e6f0f9d8b
Tibo Van Gindertaelen <tibo.vangindertaelen@student.odisee.be>
2020-10-29 16:05:54 +0100

    Code reverted to Property useage
    This commit is mostly aimed at the ability to look back on the to-be-used-practice as the established standard is the class-based property approach as apposed to the hooks (functional) approach of building Reactive components

--- 5-React-Course/react-complete-guide/src/App.js
+++ 5-React-Course/react-complete-guide/src/App.js
@@ -1,31 +1,25 @@
 // ctrl + K + C/U --- shift + alt + F
 // React re-rendering -> watching props or state
-// State-management in two ways, prop for class-based and hooks for functional
 
-// Importing components AND hooks
-import React, { useState } from 'react';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  // Hooks returns current state and setter
-  // ArrayDestructuring -> Multi-Define vars on function return (Array Destructurising)
-  // VERY IMPORTANT: Class-based MERGES as opposed to hook which REPLACES (losing properties!)
-  // Solution -> reference/specify target prop in useState call
-  const [personState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
-    ]
-  });
-
-  const [otherState, setOtherState] = useState('some other value');
+    ],
+    ohterState: 'some other value'
+  }
 
   // ES6 syntax provides correct scoping -> this-referencing possible
   // Functions nesting!
-  const switchNameAndAgeHandler = () => {
-    setPersonsState({
+  switchNameAndAgeHandler = (newName) => {
+    this.setState({
       persons: [
         { name: 'Maximillian', age: 28 },
         { name: 'Manu', age: 29 },
@@ -34,27 +28,26 @@ const app = props => {
     });
   }
 
-  return (
-    // Typically one root elem (or json) per component or as return for this render function
-    <div className="App">
-      <h1>Test</h1>
-      {/* Parentheses in the function ref would execute the function at render-runtime */}
-      <button onClick={switchNameAndAgeHandler}>Switch Names</button>
-      <Person
-        name={personState.persons[0].name}
-        age={personState.persons[0].age} />
-      <Person
-        name={personState.persons[1].name}
-        age={personState.persons[1].age} 
-        click={switchNameAndAgeHandler}>My Hobbies: Racing</Person>
-      <Person
-        name={personState.persons[2].name}
-        age={personState.persons[2].age} />
-    </div>
-  );
-
-  // JSx Compiles f.ex. to this:
-  // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Test'));
+  render () {
+    return (
+      // Typically one root elem (or json) per component or as return for this render function
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is Really Working!</p>
+        <button onClick={this.switchNameAndAgeHandler}>Switch Names</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameAndAgeHandler}>My Hobbies: Racing</Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
+      </div>
+    );
+  }
 }
 
-export default app;
+export default App;
