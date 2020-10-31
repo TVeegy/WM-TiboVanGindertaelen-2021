df966749c91583152e063c2a217377127724d9cf
Tibo Van Gindertaelen <tibo.vangindertaelen@student.odisee.be>
2020-10-31 03:35:44 +0100

    Wrapped invalid charcter

--- 5-React-Course/base-syntax--assignment-problem/src/App.js
+++ 5-React-Course/base-syntax--assignment-problem/src/App.js
@@ -56,8 +56,8 @@ class App extends Component {
           <li><del>UserInput should hold an input element, UserOutput two paragraphs</del></li>
           <li><del>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</del></li>
           <li><del>Pass a username (of your choice) to UserOutput via props and display it there</del></li>
-          <li><del>Add state to the App component (=> the username) and pass the username to the UserOutput component</del></li>
-          <li><del>Add a method to manipulate the state (=> an event-handler method)</del></li>
+          <li><del>Add state to the App component (={">"} the username) and pass the username to the UserOutput component</del></li>
+          <li><del>Add a method to manipulate the state (={">"} an event-handler method)</del></li>
           <li><del>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</del></li>
           <li><del>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</del></li>
           <li><del>Add two-way-binding to your input (in UserInput) to also display the starting username</del></li>
