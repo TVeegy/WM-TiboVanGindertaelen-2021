09f202c4c196adb4f1bf407571864963cb259f68
Tibo Van Gindertaelen <tibo.vangindertaelen@student.odisee.be>
2020-11-02 21:56:36 +0100

    Lecture 54 - Conditional Rendering

--- 5-React-Course/react-complete-guide/src/App.js
+++ 5-React-Course/react-complete-guide/src/App.js
@@ -33,8 +33,15 @@ class App extends Component {
         { name: 'Max', age: 28 },
         { name: event.target.value, age: 29 },
         { name: 'Stephanie', age: 26 }
-      ]
+      ],
+      otherStaet: 'some value',
+      showPersons: false
     });
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
   }
 
   render() {
@@ -46,6 +53,26 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameAndAgeHandler.bind(this, 'Maximillian!')}
+            changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
+        </div>
+      );
+    }
+
     return (
       // Typically one root elem (or json) per component or as return for this render function
       <div className="App">
@@ -53,18 +80,23 @@ class App extends Component {
         <p>This is Really Working!</p>
         <button
           style={style}
-          onClick={() => this.switchNameAndAgeHandler('Maximillian!!')}>Switch Names</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age} />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameAndAgeHandler.bind(this, 'Maximillian!')}
-          changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age} />
+          // Function call: onClick={() => this.togglePersonsHandler}>Switch Names</button>
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {/* Ternary expression, no block statements allowed. Use of === */}
+        { this.state.showPersons ?
+          <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age} />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              click={this.switchNameAndAgeHandler.bind(this, 'Maximillian!')}
+              changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+            <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age} />
+          </div> : null}
       </div>
     );
   }
