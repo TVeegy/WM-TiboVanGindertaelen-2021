17ed5c03aff9fc6f728c5a9ea1101e6b7df7535a
Tibo Van Gindertaelen <tibo.vangindertaelen@student.odisee.be>
2020-11-05 20:03:02 +0100

    68. Adding and using Radium

--- 5-React-Course/react-complete-guide/src/App.js
+++ 5-React-Course/react-complete-guide/src/App.js
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -53,7 +54,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -74,6 +79,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     const classes = [];
@@ -99,4 +108,5 @@ class App extends Component {
   }
 }
 
-export default App;
+// Higher order component (injection)
+export default Radium(App);
