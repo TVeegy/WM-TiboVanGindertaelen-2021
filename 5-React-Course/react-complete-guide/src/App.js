ff44aaee9beb028a38e9af45ec722ee2233b0e4c
Tibo Van Gindertaelen <tibo.vangindertaelen@student.odisee.be>
2020-11-05 19:42:46 +0100

    66. Setting styles dynamically

--- 5-React-Course/react-complete-guide/src/App.js
+++ 5-React-Course/react-complete-guide/src/App.js
@@ -48,7 +48,8 @@ class App extends Component {
   render() {
     // Defining Variables
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
