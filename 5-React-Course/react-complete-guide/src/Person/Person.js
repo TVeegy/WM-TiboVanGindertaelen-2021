0d0dca2f828ad62f8600ecf33947cbfb77f43585
Tibo Van Gindertaelen <tibo.vangindertaelen@student.odisee.be>
2020-11-06 04:52:50 +0100

    82. Wrap Up

--- 5-React-Course/react-complete-guide/src/Person/Person.js
+++ 5-React-Course/react-complete-guide/src/Person/Person.js
@@ -7,10 +7,6 @@ const person = (props) => {
 
     const rnd = Math.random();
 
-    if (rnd > 0.7) {
-        throw new Error('Something went wrong');
-    }
-
     return (
         <div className={classes.Person}>
             < p onClick={props.click} > I'm a Person and I'm {props.name} and I'm {props.age} years old!</p>
