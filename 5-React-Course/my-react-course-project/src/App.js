78d6926963680bde5ed9e2371feb3ce15488b2e9
Tibo Van Gindertaelen <46933283+TVeegy@users.noreply.github.com>
2021-02-18 19:56:25 +0100

    Lectures 31 and 32

--- 5-React-Course/my-react-course-project/src/App.js
+++ 5-React-Course/my-react-course-project/src/App.js
@@ -1,16 +1,20 @@
 // Importing generally two things; the react lib and the Component class itself
 import React, { Component } from 'react';
 import './App.css';
+import person from './Person/Person';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi I'm a React App</h1>
+        <p>This is really working!</p>
+        <Person></Person>
       </div>
     );
   }
 }
 
-// ES6 Feature; when exporting this file, you'll export this class
+// ES6 Feature;
 export default App;
