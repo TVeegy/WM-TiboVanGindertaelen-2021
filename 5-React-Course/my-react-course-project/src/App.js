9774f30bc9ad3994c2c4725c7f940a493cbf09a8
Tibo Van Gindertaelen <46933283+TVeegy@users.noreply.github.com>
2021-02-24 11:17:28 +0100

    Lecture  44 part 1

--- 5-React-Course/my-react-course-project/src/App.js
+++ 5-React-Course/my-react-course-project/src/App.js
@@ -1,44 +1,64 @@
-// Importing generally two things; the react lib and the Component class itself
-import React, { Component } from 'react';
+// React Version 16.8 --> Start Functional Components!!
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
-    persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
-    ],
-    otherState: 'some other value'
-  }
+const app = props => {
+  const [personState, setPersonsState] = useState(
+    {
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ],
+      otherState: 'some other value'
+    }
+  );
 
-  // ES6 arrow function (a property, not a function)
-  switchNameHandler = () => {
-     // console.log('Was clicked!')
-     // this.state.persons[0].name = 'Maximilian'; WILL NOT WORK
-    this.setState({
+  const [otherState, setOtherState] = useState('some other calue');
+
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
         { name: 'Maximilian', age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 27 }
       ]
-    });
-  }
-  
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi I'm a React App</h1>
-        <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name= {this.state.persons[0].name} age={this.state.persons[0].age}></Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
-      </div>
-    );
-  }
-}
+    })};
+    
+  return (
+    <div className="App">
+      <h1>Hi I'm a React App</h1>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name= {personState.persons[0].name} age={personState.persons[0].age}></Person>
+      <Person name={personState.persons[1].name} age={personState.persons[1].age}>My Hobbies: Racing</Person>
+      <Person name={personState.persons[2].name} age={personState.persons[2].age}></Person>
+    </div>
+  );
+};
 
 // ES6 Feature;
-export default App;
+export default app;
+
+// state = {
+//   persons: [
+//     { name: 'Max', age: 28 },
+//     { name: 'Manu', age: 29 },
+//     { name: 'Stephanie', age: 26 }
+//   ],
+//   otherState: 'some other value'
+// }
+
+// // ES6 arrow function (a property, not a function)
+// switchNameHandler = () => {
+//    // console.log('Was clicked!')
+//    // this.state.persons[0].name = 'Maximilian'; WILL NOT WORK
+//   this.setState({
+//     persons: [
+//       { name: 'Maximilian', age: 28 },
+//       { name: 'Manu', age: 29 },
+//       { name: 'Stephanie', age: 27 }
+//     ]
+//   });
+// }
--- 5-React-Course/my-react-course-project/src/Person/Person.js
+++ 5-React-Course/my-react-course-project/src/Person/Person.js
@@ -3,8 +3,6 @@ import React from 'react';
 const person = (props) => {
     return( 
     <div>
-        {/* You can pass external content by props or children */}
-        {/* Note: for each not-present child, an empty <p> gets rendered */}
         <p>I'm a {props.name} and I am {props.age} years old!</p>
         <p>{props.children}</p>
     </div>
