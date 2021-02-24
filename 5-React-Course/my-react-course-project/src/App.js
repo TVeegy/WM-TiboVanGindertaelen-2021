9abc9a524646ae566e69eb7bf7ede74dc3b70fe3
Tibo Van Gindertaelen <46933283+TVeegy@users.noreply.github.com>
2021-02-24 10:19:07 +0100

    Lecture 40-41

--- 5-React-Course/my-react-course-project/src/App.js
+++ 5-React-Course/my-react-course-project/src/App.js
@@ -12,12 +12,17 @@ class App extends Component {
     ]
   }
 
+  // ES6 arrow function (a property, preserving a this reference to the class and not the would-be function scope)
+  switchNameHandler = () => {
+     console.log('Was clicked!')
+  }
+  
   render() {
     return (
       <div className="App">
         <h1>Hi I'm a React App</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name= {this.state.persons[0].name} age={this.state.persons[0].age}></Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
