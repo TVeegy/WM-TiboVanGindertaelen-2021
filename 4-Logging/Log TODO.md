694b2536c4ea276fed1a464f7a3aa825c4a824c9
Tibo Van Gindertaelen <46933283+TVeegy@users.noreply.github.com>
2021-02-26 09:31:50 +0100

    Structure Update

--- 4-Logging/Log TODO.md
+++
@@ -1,8 +0,0 @@
-## 22 Oktober om 22u07 werd deze Repository opgestart!
-
-### TODO
-- Beslissen welke delen van de workflow in welke taal gebeuren:  
-  - Nederlands voor zaken die persoonlijk en beschrijvend zijn (Extra voor commits bv) 
-  - Engels voor zaken die conventie, organisatie en bevattend zijn (grootste aandeel)
-- Verwerken van gebruikte resources: (ze documenteren)  
-    - [A text file](TODO-Files/TODO_used_resources.txt)   
--- 5-React-Course/my-react-course-project/src/App.js
+++ 5-React-Course/my-react-course-project/src/App.js
@@ -1,18 +1,64 @@
-// Importing two things; React as a mechanic and the Comp class
-import React, { Component } from 'react';
+// React Version 16.8 --> Start Functional Components!!
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-// Gets rendered by the index.js file (NOTE: the index file is js and not html, rendering one root comp nesting all else)
-class App extends Component {
-  // each comp has to return to-be-rendered pseudo html
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi I'm a React App</h1>
-      </div>
-    );
-  }
-}
+const app = props => {
+  const [personState, setPersonsState] = useState(
+    {
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ],
+      otherState: 'some other value'
+    }
+  );
 
-// ES6 feature: if you imp this file, you imp this class
-export default App;
+  const [otherState, setOtherState] = useState('some other calue');
+
+  const switchNameHandler = () => {
+    setPersonsState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    })};
+    
+  return (
+    <div className="App">
+      <h1>Hi I'm a React App</h1>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name= {personState.persons[0].name} age={personState.persons[0].age}></Person>
+      <Person name={personState.persons[1].name} age={personState.persons[1].age}>My Hobbies: Racing</Person>
+      <Person name={personState.persons[2].name} age={personState.persons[2].age}></Person>
+    </div>
+  );
+};
+
+// ES6 Feature;
+export default app;
+
+// state = {
+//   persons: [
+//     { name: 'Max', age: 28 },
+//     { name: 'Manu', age: 29 },
+//     { name: 'Stephanie', age: 26 }
+//   ],
+//   otherState: 'some other value'
+// }
+
+// // ES6 arrow function (a property, not a function)
+// switchNameHandler = () => {
+//    // console.log('Was clicked!')
+//    // this.state.persons[0].name = 'Maximilian'; WILL NOT WORK
+//   this.setState({
+//     persons: [
+//       { name: 'Maximilian', age: 28 },
+//       { name: 'Manu', age: 29 },
+//       { name: 'Stephanie', age: 27 }
+//     ]
+//   });
+// }
